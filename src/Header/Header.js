639ba80895d4e2b74d790b460b8c8733495d8424
sanvisimo <sanvi.simo@gmail.com>
2023-02-06 16:12:09 +0100

    clean props

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,15 +3,12 @@ import MainLogo from "../000-img/000-FV-logotipo-wide.svg"
 import './Header.css'
 
 
-export const Menu = ({paths, name, ordine, title}) => {
+export const Menu = ({paths}) => {
 
 
-    return <>
-            {title && <div>{title}</div>}
-            <ul>
-                {paths.map((path, index) => (<li key={path.path} id={`menu-el-${index}`}><a href={path.path}>{path.title}</a></li> ))}                    
-            </ul>
-    </>
+    return <ul>
+            {paths.map((path, index) => (<li key={path.path} id={`menu-el-${index}`}><a href={path.path}>{path.title}</a></li> ))}                    
+        </ul>
 }
 
 
@@ -40,10 +37,7 @@ const Header = () => {
         <header className="Header">
             <img src={MainLogo} alt="Main Logo" />
             <Menu 
-                paths={routes} 
-                name="il mio menù" 
-                ordine={1} 
-                title='title'
+                paths={routes}                 
             />
         </header>
     )
