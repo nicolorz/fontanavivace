f34a4714ec73a2398ef2cc0743d2486ffc2cd546
sanvisimo <sanvi.simo@gmail.com>
2023-02-06 16:11:29 +0100

    first time props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
     </div>
   );
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,18 +2,49 @@ import React from "react";
 import MainLogo from "../000-img/000-FV-logotipo-wide.svg"
 import './Header.css'
 
+
+export const Menu = ({paths, name, ordine, title}) => {
+
+
+    return <>
+            {title && <div>{title}</div>}
+            <ul>
+                {paths.map((path, index) => (<li key={path.path} id={`menu-el-${index}`}><a href={path.path}>{path.title}</a></li> ))}                    
+            </ul>
+    </>
+}
+
+
 const Header = () => {
+    const routes = [
+        {
+            path: 'chi-siamo',
+            title: 'Chi siamo'
+        },
+        {
+            path: 'casa-famiglia',
+            title: 'La casa famiglia'
+        },
+        {
+            path: 'progetti',
+            title: 'Progetti'
+        },
+        {
+            path: 'contatti',
+            title: 'Contatti'
+        },
+    ]
+    
+
     return  (
         <header className="Header">
             <img src={MainLogo} alt="Main Logo" />
-            <ul>
-                <li><a href="#">Chi siamo</a></li>
-                <li><a href="#">La casa famiglia</a></li>
-                <li><a href="#">Progetti</a></li>
-                <li><a href="#">Contatti</a></li>
-                <li><a href="#">Blog</a></li>
-                
-            </ul>
+            <Menu 
+                paths={routes} 
+                name="il mio menù" 
+                ordine={1} 
+                title='title'
+            />
         </header>
     )
 }
